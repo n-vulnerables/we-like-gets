eeab5e376a1913b1eb920d133c502b1121f9df2f
Nathan <nathan.peercy@praetorian.com>
2021-11-17 09:44:43 -0800

    Create gets.c

---
+++ gets.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+//https://faq.cprogramming.com/cgi-bin/smartfaq.cgi?answer=1049157810&id=1043284351
+typedef struct MyStruct
+{
+  char buf[5];
+  int  i;
+} MyStruct_t;
+
+int main(void)
+{
+  MyStruct_t my;
+  
+  my.i = 10;
+  
+  printf ("my.i is %d\n", my.i);
+  printf ("Enter a 10 digit number:");  /* Too big on purpose  */
+  
+  gets(my.buf);
+  
+  printf ("my.buf is >%s<\n", my.buf);
+  printf ("my.i is %d\n", my.i);
+  
+  return(0);
+}
